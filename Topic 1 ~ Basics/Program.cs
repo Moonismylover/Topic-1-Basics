78f3e4fadac589d379707e7ccc0cdb5eca74111f
700014473 <700014473@2260I-L29-026>
2026-09-09 11:55:44 -0400

    Add project files.

---
+++ Topic 1 ~ Basics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_1___Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello World
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine();
+
+            // Assigning a value to a variable
+            string aFriend = "Bill";
+            Console.WriteLine(aFriend);
+
+            Console.WriteLine();
+
+            // Reassigning a value to a variable
+            aFriend = "Maria";
+            Console.WriteLine(aFriend);
+
+            Console.WriteLine();
+
+            // Sentence with variable and string
+            Console.WriteLine("Hello " + aFriend);
+            Console.WriteLine($"Hello {aFriend}");
+
+
+
+        }
+    }
+}
